cce3ecef3bad0c90a4d59979694dd20bbc185a35
Phil <code4life1337@gmail.com>
2022-11-18 17:36:25 +0100

    script added plus other updates

---
+++ script.js
@@ -0,0 +1,11 @@
+ let projectOne = document.getElementsByClassName('project1')
+ let projectTwe = document.getElementsByClassName('project2')
+ let projectThree = document.getElementsByClassName('project3')
+
+
+
+function openClose1(){
+    projectOne.style.display = 'none';
+}
+
+ projectOne.addEventListener('click', openClose1)
